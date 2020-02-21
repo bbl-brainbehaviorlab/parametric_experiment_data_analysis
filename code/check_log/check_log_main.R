b11475742fbdd7ac2e6fa8f9a8497e00848950c5
HeidiChiu <heidichiu1025@gmail.com>
2020-02-21 14:01:30 +0800

    Add a loop to check for all files

--- code/check_log/check_log_main.R
+++ code/check_log/check_log_main.R
@@ -1,20 +1,25 @@
 library(tidyverse)
-
 source(here::here("code/check_log/check_log_functions.R"))
-path = "~/Dropbox/Working/01_EntrainmentProject/0_ExperimentParametric/03_ExperimentCoding/important_log_archive/heidi_test_v1_2"
-file = "13-parametric_exp.log"
 
-raw <-  read.csv(file.path(path,file),skip=3)
+path <-  "~/Dropbox/Working/01_EntrainmentProject/0_ExperimentParametric/03_ExperimentCoding/important_log_archive/heidi_test_v1_2"
+files <- list.files(path = path, full.names = F)
 
+preceeding_standards_all <- NULL
 
+for (file in files){
+  raw <-  read.csv(file.path(path,file),skip=3)
+  
+  picture <- raw %>% 
+    filter(Event.Type == "Picture" & counter_balance_id.str.!= "88888" & counter_balance_id.str.!= "Instruction_Page") %>% 
+    mutate(duration_diff = Duration/10 - wav_file_duration.str.) %>% 
+    cleanColName()
+  
+  picture %>% 
+    run_all_check()
+  
+  preceeding_standards <- picture %>% 
+    mean_num_preceeding_standard()
+  
+  preceeding_standards_all <- rbind(preceeding_standards_all, preceeding_standards)
+}
 
-picture <- raw %>% 
-  filter(Event.Type == "Picture" & counter_balance_id.str.!= "88888" & counter_balance_id.str.!= "Instruction_Page") %>% 
-  mutate(duration_diff = Duration/10 - wav_file_duration.str.) %>% 
-  cleanColName()
-
-picture %>% 
-  run_all_check()
-
-preceeding_standards <- picture %>% 
-  mean_num_preceeding_standard()
